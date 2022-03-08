530834add9ddf2329d6a48f530b02e9c163b0f8d
Hipp Alexander <l0522@schueler.ms-muc.de>
2022-03-08 09:08:08 +0100

    Eingang I2 Erstellt

--- TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
@@ -21,6 +21,7 @@ static const char * const TAG = "EXAMPLE_TON";
 
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
+#define BUTTON_I2 GPIO_NUM_32        // Pin 32.
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
 
 
@@ -40,9 +41,11 @@ extern "C" void app_main(void)
     */
     gpio_reset_pin(GPIO_Q1);
     gpio_reset_pin(BUTTON_I1);
+    gpio_reset_pin(BUTTON_I2);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
 
 
@@ -54,12 +57,16 @@ extern "C" void app_main(void)
     {
         // Eingang lesen, das not wird gebraucht weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = not gpio_get_level(BUTTON_I1);
+        bool I2 = not gpio_get_level(BUTTON_I2);
 
         // den I1 an TON1 uebergeben, und TON1 aufrufen
         TON1(I1);
 
+        TOGGLE1.RST = I2;
+               TOGGLE1( TON1.Q);
+
         // Ausgaenge setzen
-        gpio_set_level(GPIO_Q1, TON1.Q);
+        gpio_set_level(GPIO_Q1,TOGGLE1.Q);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
